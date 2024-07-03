2ce82ab4fee75ef99c4dc09edc17afdb0dfcca7b
SantiagoOrtega22 <104149460+SantiagoOrtega22@users.noreply.github.com>
2024-07-02 23:20:50 -0500

    Update ListadoProductos.jsx

--- shopping-cart/src/PanelAdmin/components/ListadoProductos.jsx
+++ shopping-cart/src/PanelAdmin/components/ListadoProductos.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import '../Styles/ListadoProductos.css';
-
+import Axios from 'axios';
 const ListadoProductos = () => {
   const [productos, setProductos] = useState([]); 
 
   // Fetch cart data from localStorage on component mount
   useEffect(() => {
-    const products = JSON.parse(localStorage.getItem('cartCopy'));
-    if (products) {
-      setProductos(products);
+    const idUsuario = JSON.parse(localStorage.getItem('newDatos'));
+    Axios.post("http://localhost:3001/carrito",{ 
+      id_orden:idUsuario,}
+  ).then((response)=>{
+    if (productos) {
+      setProductos(response.data);
     }
+  })
+    
   }, []);
 
   const LimpiarProductos = () => {
